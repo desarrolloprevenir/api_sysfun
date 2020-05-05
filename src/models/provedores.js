e3fe7482d6d8ad9f15cdcafa5b6775ed5cf07b7f
juank8911 <juancarlos_891111@hotmail.com>
2020-05-05 17:29:01 -0500

    agrgegando node mmodule

---
+++ src/models/provedores.js
@@ -0,0 +1,29 @@
+let mysql = require('mysql');
+let config = require('../config');
+
+connection = mysql.createConnection({
+host: config.domain,
+user: config.userbd,
+password: config.passwordbd,
+database: config.nombredb
+});
+
+let provmodel = {};
+
+provmodel.registraProvedor = (provedor,callback) => {
+  if(connection)
+  {
+    console.log(provedor);
+    let sql = 'INSERT INTO provedores (nit, codigo, nombre, direccion, telefono, celular) VALUES (?, ?, ?, ?, ?, ?);';
+    connection.query(sql,[provedor.nit,provedor.codigo,provedor.nombre,provedor.direccion,provedor.telefono,provedor.celular],(err,resp)=>{
+      if(err){throw err}
+      else {
+        console.log(resp);
+        callback(null,true);
+      }
+    })
+  }
+};
+
+
+module.exports = provmodel;
